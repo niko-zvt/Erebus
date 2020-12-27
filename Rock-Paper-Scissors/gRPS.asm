af64d7a8b2265c89b92c379051949f7ade82a79a
Nikolay Zhivotenko <niko.zvt@gmail.com>
2020-12-27 15:02:32 +0300

    Validate the input data

--- Rock-Paper-Scissors/gRPS.asm
+++ Rock-Paper-Scissors/gRPS.asm
@@ -50,28 +50,52 @@ global      _start
 ;; ----------------------------------------------
 section     .text
 
-;; Entry point                                                 
+            ;; Entry point                                                 
 _start:     PRINT       "Hello, *USERNAME*! "                       ;; Macro for displaying the greeting line
             PRINT       "This is a Rock, Paper & Scissors ASM game!";; Macro for displaying the greeting line
             PUTCHAR     10                                          ;; Macro for displaying a newline character (LF/0x0A/10)
-            PUTCHAR     10                                          ;; LF
+
+            ;; Entering the number of rounds
+inputRnds:  PUTCHAR     10                                          ;; LF
             PRINT       "How many rounds do you want to play? "     ;; Macro for displaying a prompt to enter the number of rounds
             PRINT       "Please enter a number from 1 to 9: "       ;; Macro for displaying a prompt to enter the number of rounds
             GETCHAR                                                 ;; Macro for entering an ASCII character in the EAX register
-            ;; TODO                                                 ;; Checking digit input
             mov         [numRnds],  eax                             ;; Moving the entered character from EAX to a variable
+
+            ;; Checking that the entered character is 'Q'
+            mov         edx,        81                              ;; Moving 'Q' symbol to EDX
+            cmp         edx,        [numRnds]                       ;; Comparing the number of rounds with 'Q'
+            je          terminate                                   ;; If the entered character is 'Q', then go to the label "terminate"
+
+            ;; Checking that the entered character is 'q'
+            mov         edx,        113                             ;; Moving 'q' symbol to EDX
+            cmp         edx,        [numRnds]                       ;; Comparing the number of rounds with 'q'
+            je          terminate                                   ;; If the entered character is 'q', then go to the label "terminate"
+
+            ;; Shift the entered character in the ASCII table
             sub         [numRnds],  dword   48                      ;; Shift the entered character in the ASCII table to get a digit
+
+            ;; Checking that the entered character is less than 1
+            mov         edx,        1                               ;; Moving 1 to EDX
+            cmp         edx,        [numRnds]                       ;; Comparing the number of rounds with 1
+            jg          inputRnds                                   ;; If the number of rounds is less than 1 then go to the label "inputRnds"
+
+            ;; Checking that the entered character is more than 9
+            mov         edx,        9                               ;; Moving 9 to EDX
+            cmp         edx,        [numRnds]                       ;; Comparing the number of rounds with 9
+            jl          inputRnds                                   ;; If the number of rounds is more than 9 then go to the label "inputRnds"
+
             mov         ecx,        0                               ;; Reset the ECX register, which will be the counter
             PUTCHAR     10                                          ;; LF
 
-;; The main game loop
+            ;; The main game loop
 main_loop:  PRINT       "Main Loop"                                 ;; Print temp text "Main loop"
             PUTCHAR     10                                          ;; LF
             ;; TODO
             inc         ecx                                         ;; Increasing the value of the ECX register (counter) by one
             cmp         ecx,        [numRnds]                       ;; Comparing the counter with the number of rounds
             jl          main_loop                                   ;; If ECX < [numRnds] jump to "main_loop" lable
-            FINISH                                                  ;; Else process terminate
+terminate:  FINISH                                                  ;; Else process terminate
 
 ;; ----------------------------------------------
 ;; Section BSS
